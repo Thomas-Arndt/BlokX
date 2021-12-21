5ab090a461b17893c456f72ebe608a01fd3a1e8a
Thomas Arndt <arndtt42@gmail.com>
2021-12-21 14:22:58 -0800

    Cleanup of unnecessary routes

--- flask_app/controllers/controller_routes.py
+++ flask_app/controllers/controller_routes.py
@@ -11,25 +11,6 @@ from flask_app.models.model_transaction import Transaction
 def index():
     return render_template("login.html")
 
-@app.route('/test')
-def test():
-    return render_template("index.html")
-
-@app.route('/add/txn')
-def add_txn():
-    # txn={
-    #         "sender": "A",
-    #         "receiver": "B",
-    #         "amount": random.randint(0, 1000),
-    #         "timestamp": str(datetime.now())
-    #         }
-    user_1=f"user{random.randint(0,100)}@email.com"
-    user_2=f"user{random.randint(0,100)}@email.com"
-    txn=Transaction(user_1, user_2, random.randint(0, 1000), "This is a test transaction.")
-    MINER.add_new_transaction(txn)
-    return redirect('/')
-
-
 # @app.errorhandler(404)
 # def server_error(e):
 #     print("Running error function...")
